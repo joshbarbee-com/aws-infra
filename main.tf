cb1b07b3067d6bea082d9a613ca27071bf2d1432
Josh Barbee <joshbarbee1@gmail.com>
2025-03-02 17:56:18 -0500

    use lockfile

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
         bucket         = "terraform-state-bucket"
         key            = "terraform.tfstate"
         region         = "us-east-2"
-        dynamodb_table = "terraform_locks"
+        use_lockfile = true
     }
 }
 
@@ -16,15 +16,5 @@ resource "aws_s3_bucket" "terraform-state" {
 
     lifecycle {
         prevent_destroy = true
-    }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-    name           = "terraform_locks"
-    billing_mode   = "PAY_PER_REQUEST"
-    hash_key       = "LockID"
-    attribute {
-        name = "LockID"
-        type = "S"
     }
 }
