b2405df41c8094eb7c73a0356244176047782e79
Josh Barbee <joshbarbee1@gmail.com>
2025-03-05 18:09:33 -0500

    fix acm record

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_route53_record" "r53-txt-record" {
 
 resource "aws_route53_record" "r53-cname-record-acm" {
     for_each = {
-        for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+        for dvo in aws_acm_certificate.acm-cert.domain_validation_options : dvo.domain_name => {
         name   = dvo.resource_record_name
         record = dvo.resource_record_value
         type   = dvo.resource_record_type
