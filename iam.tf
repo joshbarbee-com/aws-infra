3e0d88dad058273099a14b8cc0d61e2272f3d397
Josh Barbee <joshbarbee1@gmail.com>
2025-03-05 18:26:32 -0500

    adding acm / iam roles

--- iam.tf
+++ iam.tf
@@ -68,6 +68,52 @@ data "aws_iam_policy_document" "terraform-route53-records" {
     }
 }
 
+data "aws_iam_policy_document" "terraform-acm" {
+    statement {
+        sid = "ACMRequestCertificate"
+        effect = "Allow"
+        actions = [
+            "acm:RequestCertificate",
+            "acm:DescribeCertificate",
+            "acm:ListCertificates",
+            "acm:GetCertificate",
+            "acm:DeleteCertificate"
+        ]
+        resources = [aws_acm_certificate_validation.acm-cert.arn]
+    }
+}
+
+data "aws_iam_policy_document" "terraform-iam-roles" {
+    statement {
+        sid = "IAMRoles"
+        effect = "Allow"
+        actions = [
+            "iam:CreateRole",
+            "iam:DeleteRole",
+            "iam:GetRole",
+            "iam:ListRoles",
+            "iam:UpdateRole",
+            "iam:PassRole"
+        ]
+        resources = ["*"]
+    }
+}
+
+resource "aws_iam_policy" "terraform-iam-roles" {
+    name   = "terraform-iam-roles"
+    policy = data.aws_iam_policy_document.terraform-iam-roles.json
+}
+
+resource "aws_iam_role_policy_attachment" "github-cicd-iam-roles-attach" {
+    role       = aws_iam_role.github-cicd.name
+    policy_arn = aws_iam_policy.terraform-iam-roles.arn
+}
+
+resource "aws_iam_policy" "terraform-acm" {
+    name   = "terraform-acm"
+    policy = data.aws_iam_policy_document.terraform-acm.json
+}
+
 resource "aws_iam_policy" "terraform-route53-records" {
     name   = "terraform-route53-records"
     policy = data.aws_iam_policy_document.terraform-route53-records.json
@@ -96,4 +142,9 @@ resource "aws_iam_role_policy_attachment" "github-cicd-route53-hostedzone-attach
 resource "aws_iam_role_policy_attachment" "github-cicd-route53-records-attach" {
     role       = aws_iam_role.github-cicd.name
     policy_arn = aws_iam_policy.terraform-route53-records.arn
+}
+
+resource "aws_iam_role_policy_attachment" "github-cicd-acm-attach" {
+    role       = aws_iam_role.github-cicd.name
+    policy_arn = aws_iam_policy.terraform-acm.arn
 }
