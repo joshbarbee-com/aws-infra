4973a7f4af38261d8d1b7293d3b10803d223a6a4
Josh Barbee <joshbarbee1@gmail.com>
2025-03-28 17:05:32 -0400

    Fix SNS alert ARN

--- sns.tf
+++ sns.tf
@@ -2,14 +2,14 @@ resource "aws_sns_topic" "global-alerts" {
     name = "global-alerts"
 }
 
-resource "aws_sns_topic_subscription" "email_alerts" {
-    topic_arn = aws_sns_topic.alerts.arn
+resource "aws_sns_topic_subscription" "email-alerts" {
+    topic_arn = aws_sns_topic.global-alerts.arn
     protocol  = "email"
     endpoint  = var.email
 }
 
-resource "aws_sns_topic_subscription" "sms_alerts" {
-    topic_arn = aws_sns_topic.alerts.arn
+resource "aws_sns_topic_subscription" "sms-alerts" {
+    topic_arn = aws_sns_topic.global-alerts.arn
     protocol  = "sms"
     endpoint  = var.phone_number
 }
