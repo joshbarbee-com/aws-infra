bedf60393953ca10dae0ea5a8774c43b68ab6c4f
Josh Barbee <joshbarbee1@gmail.com>
2025-03-28 19:13:23 -0400

    Remove aws redirect from infra

--- r53.tf
+++ r53.tf
@@ -11,19 +11,6 @@ resource "aws_acm_certificate_validation" "acm-cert-validation" {
     validation_record_fqdns  = [for record in aws_route53_record.r53-cname-record-acm : record.fqdn]
 }
 
-resource "aws_acm_certificate" "acm-cert-aws" {
-    domain_name       = "aws.${var.domain_name}"
-    validation_method = "DNS"
-    lifecycle {
-      create_before_destroy = true
-    }
-}
-
-resource "aws_acm_certificate_validation" "acm-cert-validation-aws" {
-    certificate_arn          = aws_acm_certificate.acm-cert-aws.arn
-    validation_record_fqdns  = [for record in aws_route53_record.r53-cname-record-acm-aws : record.fqdn]
-}
-
 resource "aws_route53_zone" "r53-hosted-zone" {
     name = var.domain_name
 }
@@ -40,23 +27,6 @@ resource "aws_route53_record" "r53-txt-record" {
 resource "aws_route53_record" "r53-cname-record-acm" {
     for_each = {
         for dvo in aws_acm_certificate.acm-cert.domain_validation_options : dvo.domain_name => {
-        name   = dvo.resource_record_name
-        record = dvo.resource_record_value
-        type   = dvo.resource_record_type
-        }
-    }
-
-    allow_overwrite = true
-    name            = each.value.name
-    records         = [each.value.record]
-    ttl             = 360
-    type            = each.value.type
-    zone_id         = aws_route53_zone.r53-hosted-zone.zone_id
-}
-
-resource "aws_route53_record" "r53-cname-record-acm-aws" {
-    for_each = {
-        for dvo in aws_acm_certificate.acm-cert-aws.domain_validation_options : dvo.domain_name => {
         name   = dvo.resource_record_name
         record = dvo.resource_record_value
         type   = dvo.resource_record_type
