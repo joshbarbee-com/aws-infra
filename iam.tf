814940506b16a3eafafb8011c6d434bdc510e5f3
Josh Barbee <joshbarbee1@gmail.com>
2025-03-28 19:44:04 -0400

    Permission updates for provisioner s3 bucket

--- iam.tf
+++ iam.tf
@@ -24,14 +24,14 @@ data "aws_iam_policy_document" "terraform-infra-s3" {
     statement {
         sid = "ListBucket"
         effect = "Allow"
-        actions = ["s3:ListBucket"]
+        actions = ["s3:*"]
         resources = ["arn:aws:s3:::*"]
     }
 
     statement {
         sid = "GetObject"
         effect = "Allow"
-        actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+        actions = ["s3:*"]
         resources = ["arn:aws:s3:::*/*"]
     }
 }
@@ -155,4 +155,74 @@ resource "aws_iam_role_policy_attachment" "github-cicd-acm-attach" {
 resource "aws_iam_role_policy_attachment" "github-cicd-sns-attach" {
     role       = aws_iam_role.github-cicd.name
     policy_arn = aws_iam_policy.terraform-sns.arn
+}
+
+resource "aws_iam_role" "provisioner" {
+    name = "${var.terraform_role_name}-provisioner"
+    assume_role_policy = data.aws_iam_policy_document.provisioner-trust-policy.json
+}
+
+data "aws_iam_policy_document" "provisioner-trust-policy" {
+    statement {
+      actions = [ "sts:AssumeRoleWithWebIdentity"]
+      effect = "Allow"
+      principals {
+        type = "Federated"
+        identifiers = ["arn:aws:iam::${var.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+      }
+      condition {
+        test = "StringEquals"
+        variable = "token.actions.githubusercontent.com:sub"
+        values = [
+          for repo in jsondecode(file("./repos.json")) : "repo:${var.github_org}/${repo.repo_name}:ref:refs/heads/main"
+        ]
+      }
+    }
+}
+
+data "aws_iam_policy_document" "provisioner-iam-roles" {
+    statement {
+        sid = "ProvisionerIAMRoles"
+        effect = "Allow"
+        actions = [
+            "iam:CreateRole",
+            "iam:DeleteRole",
+            "iam:GetRole",
+            "iam:ListRoles",
+            "iam:UpdateRole",
+            "iam:PassRole",
+            "iam:CreatePolicy",
+            "iam:DeletePolicy",
+            "iam:GetPolicy",
+            "iam:ListRolePolicies",
+            "iam:UpdatePolicy",
+            "iam:PassPolicy",
+            "iam:GetPolicyVersion",
+            "iam:ListAttachedRolePolicies",
+            "iam:AttachRolePolicy",
+            "iam:ListPolicyVersions"
+        ]
+        resources = ["*"]
+    }
+}
+
+data "aws_iam_policy_document" "provisioner-s3" {
+    statement {
+        sid = "ProvisionerS3"
+        effect = "Allow"
+        actions = [
+            "s3:*",
+        ]
+        resources = ["arn:aws:s3:::*"]
+    }
+}
+
+resource "aws_iam_policy" "provisioner-iam-roles" {
+    name   = "provisioner-iam-roles"
+    policy = data.aws_iam_policy_document.provisioner-iam-roles.json
+}
+
+resource "aws_iam_policy" "provisioner-s3" {
+    name   = "provisioner-s3"
+    policy = data.aws_iam_policy_document.provisioner-s3.json
 }
